38ef4cf5ace377db7b17ecd57433309214ddee7a
mohamed khalil <khemeri.mohamed.khalil.simplon@gmail.com>
2020-04-24 15:39:13 +0100

    just nothig (to start npm)

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import InterSimp from "./khalil/InterSimp"; //to test InterSimp component
 import Descript from "./khalil/Descript";
+import Jess from "./khalil/Jess";
 
 function App() {
   return (
     <React.StrictMode>
+      <Jess />
       <Descript 
       title = "Presentation"
-      paragraph = "Simplon.co est un réseau de fabriques numériques et inclusives à travers le monde souhaitant faire du numérique un véritable levier d’inclusion mais également révéler des talents parmi des publics peu représentés dans le secteur. 
-            Lancée en France en 2013, elle ouvre aujourd’hui ses portes au cœur du centre de Tunis avec le soutien de la Fondation Société Générale et la Région Île de France, 
+      paragraph = "Simplon.co est un réseau de fabriques numériques et inclusives à travers le monde souhaitant faire du numérique un véritable levier d’inclusion mais également révéler des talents parmi des publics peu représentés dans le secteur.
+            Lancée en France en 2013, elle ouvre aujourd’hui ses portes au cœur du centre de Tunis avec le soutien de la Fondation Société Générale et la Région Île de France, <br />
             Simplon Tunisie lance sa deuxième promotion de futurs Développeurs.se Web Mobile au coeur du centre ville de Tunis, à Le15. Une école inclusive, visant à former les demandeurs d’emploi et contribuer ainsi au développement d’un 
             capital humain qualifié et l’inclusion d’un public éloigné de l’emploi."
       img_url= "https://i.ytimg.com/vi/Jql4Z6ob_tw/hqdefault.jpg"
--- src/khalil/InterSimp.js
+++ src/khalil/InterSimp.js
@@ -41,11 +41,11 @@ function InterSimp() {
               <p>
                 <span className="number">89% </span>sorties positives{" "}
                 <span className="operators"> = </span> <br />
-                <span class="number">71% </span> des sorties vers l'emploi{" "}
-                <span class="operators"> + </span>{" "}
-                <span class="number">9% </span> des sories vers
-                l'entrepreneuriat <span class="operators"> + </span>{" "}
-                <span class="number">9%</span> de sorties formation
+                <span classeName="number">71% </span> des sorties vers l'emploi{" "}
+                <span classeName="operators"> + </span>{" "}
+                <span classeName="number">9% </span> des sories vers
+                l'entrepreneuriat <span classeName="operators"> + </span>{" "}
+                <span classeName="number">9%</span> de sorties formation
               </p>
             </div>
           </div>
