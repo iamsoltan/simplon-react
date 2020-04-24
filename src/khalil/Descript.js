7e172e8eb2c44ad3d27676433ca4f235beb9dcee
mohamed khalil <khemeri.mohamed.khalil.simplon@gmail.com>
2020-04-24 15:09:31 +0100

    Descript component done successfully

--- src/khalil/Descript.js
+++ src/khalil/Descript.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import './Descript.css';
 
-
-function Descript() {
-  return (
-    
-
-
-
-    <section class="d-flex justify-content-between image">
-      <div>
-        <h2> Presentation </h2>
-        <p>Simplon.co est un réseau de fabriques numériques et inclusives à travers le monde souhaitant faire du numérique un véritable levier d’inclusion mais également révéler des talents parmi des publics peu représentés dans le secteur. 
-            Lancée en France en 2013, elle ouvre aujourd’hui ses portes au cœur du centre de Tunis avec le soutien de la Fondation Société Générale et la Région Île de France, 
-            Simplon Tunisie lance sa deuxième promotion de futurs Développeurs.se Web Mobile au coeur du centre ville de Tunis, à Le15. Une école inclusive, visant à former les demandeurs d’emploi et contribuer ainsi au développement d’un 
-            capital humain qualifié et l’inclusion d’un public éloigné de l’emploi.</p>
-        </div>  
-     <div  class="left" ><img src ="image/IMG_0428.JPG" alt="imag" width="70%"/></div></section>
-
-
-
-
-
-  );
+/* how to use :
+props : 
+title : the title in pink
+paragraph : the content in the paragraph
+right_left : put "right" if you want image at the right ...
+*/
+function Descript(props) {
+  if (props.right_left === "right") {
+    return (
+      <section className="outerContainer">
+        <div className="innerContainer">
+        <div className="txt">
+          <h2> {props.title} </h2>
+          <p>{props.paragraph}</p>
+          </div>
+       <div  className="img" ><img src ={props.img_url} alt="imag" /></div>
+       </div>   
+       </section>
+    );
+  }
+  if (props.right_left === "left") {
+    return (
+      <section className="outerContainer">
+        <div className="innerContainer">
+        <div  className="img" ><img src ={props.img_url} alt="imag" /></div>
+        <div className="txt">
+          <h2> {props.title} </h2>
+          <p>{props.paragraph}</p>
+          </div>
+       </div>   
+       </section>
+    );
+  }
 }
 
 export default Descript;
