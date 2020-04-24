79e6983ecac785200b5dc02458249c6e4253fb72
oussamahassani <hassani20120@gmail.com>
2020-04-24 11:28:38 +0100

    hh

--- src/App.js
+++ src/App.js
@@ -7,40 +7,6 @@ import './App.css';
 import Slider from './staff/staff'
 import Map from './staff/map'
 import Contact  from './staff/contactfrom'
-class  App extends  React.Component{
-  constructor(props) {
-
-
-    super(props);
-  }
-   state = {name:''};
-
- 
-  handelChange = (e) => {
-    this.setState ({
-     name : e.target.value 
-    
-    })
-  }
-  handelSubmit =(e) =>{
-     e.preventDefault();
-     
-     console.log(this.state.name)
-  }
-  render() {
-  return (
-    <div className="App">
-      <form onSubmit={this.handelSubmit}>
-         
-      <input type="text"/>
-         <input type="submit" value="send" onClick={this.handelChange}/>
-      </form>
-      <p> {this.state.name}</p>
-      <Map/>
-      <Slider/>
-      <Contact/>
-
-
 const links = [
 
   { name: "Acceuil", link: "##" },
@@ -58,13 +24,19 @@ function App() {
 
       <Navbar links={links} />
       <Flex />
+      <Map/>
+ <Slider/>
+ <Contact titre={presentation}/>
       <Footer links={links} />
+  
+  
+  
 
     </div>
 
   );
 }
-}
+
 
 
 export default App;
--- src/flex-photo-text/flex-photo-text.js
+++ src/flex-photo-text/flex-photo-text.js
@@ -6,7 +6,7 @@ function Flex(props) {
     return (
         <div className="flex-presentation">
             <div>
-                <h2 className="presentation-text-titre">Presentation</h2>
+                <h2 className="presentation-text-titre">{propos.titre}</h2>
                 <p className="presentation-text-paragraphe">Simplon.co est un réseau de fabriques numériques et inclusives à travers le monde souhaitant faire du numérique un véritable levier d’inclusion mais également révéler des talents parmi des publics peu représentés dans le secteur. Lancée en France en 2013, elle ouvre aujourd’hui ses portes au cœur du centre de Tunis avec le soutien de la Fondation Société Générale et la Région Île de France, Simplon Tunisie lance sa deuxième promotion de futurs Développeurs.se Web Mobile au coeur du centre ville de Tunis, à Le15. Une école inclusive, visant à former les demandeurs d’emploi et contribuer ainsi au développement d’un capital humain qualifié et l’inclusion d’un public éloigné de l’emploi.</p>
             </div>
             <div>
